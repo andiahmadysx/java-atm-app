ca0629543e1b34f22d459e94007c4ed8b607a2c6
Andi Ahmad Yusup <andiahmadyusup@gmail.com>
2023-01-25 22:42:42 +0700

    Study Case Java - Simple ATM APP

---
+++ src/App.java
@@ -0,0 +1,118 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class App {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static String name;
+
+    public static int balance = 0;
+
+    public static void main(String[] args) {
+
+
+        // Masukan username
+        // Default saldo = Rp. 0;
+        // Menampilkan Menu ( Welcom etc, nama user, saldo sekarang, menu tambah sama tarik saldo)
+        // menu tambah,set var saldo dari inputan
+
+        viewShowBalance();
+
+    }
+
+    /**
+     * Handle Menu
+     */
+
+    public static String input(String info){
+
+        System.out.print(info);
+        String value = scanner.nextLine();
+        return value;
+    }
+    public static boolean handleOption(String param){
+        if(param.equals("y") || param.equals("Y")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * BUSSINESS LOGIC
+     */
+
+    public static void showBalance(){
+
+        System.out.println("Nama Anda : " + name);
+        System.out.println("Saldo Sekarang = Rp. " + balance);
+
+
+    }
+    public static int addBalance(int value){
+
+
+        return balance += value;
+
+    }
+    public static int withdrawBalance(int value){
+
+       if(value > balance){
+           System.out.println("Gagal menarik saldo");
+           return balance;
+       } else {
+           return balance -= value;
+       }
+
+    }
+
+    /**
+     * VIEW
+     */
+
+    public static void viewShowBalance(){
+
+        System.out.println("### SELAMAT DATAND DI APP ATM SEDERHANA ###");
+        name = input("Masukan Nama : ");
+
+        balance += Integer.parseInt(input("Saldo Awal Anda : "));
+
+        System.out.println("Nama Anda = " + name);
+        System.out.println("Saldo Awal = Rp. " + balance);
+
+        boolean handleBool = true;
+
+        while (handleBool){
+            System.out.println("PILIH MENU");
+            System.out.println("1. Menabung");
+            System.out.println("2. Tarik Tabungan");
+            var option = input("Pilihan Anda : ");
+
+            switch (option){
+                case "1" :
+                    viewAddBalance();
+                    showBalance();
+                   handleBool = viewRepeatOption();
+                    break;
+                case "2" :
+                    viewWithdrawBalance();
+                    showBalance();
+                    handleBool = viewRepeatOption();
+                    break;
+                default:
+                    System.out.println("Pilihan tidak diketahui");
+            }
+        }
+
+    }
+    public static void viewAddBalance(){
+        addBalance(Integer.parseInt(input("Masukan Nominal Tabungan : ")));
+    }
+    public static void viewWithdrawBalance(){
+        withdrawBalance(Integer.parseInt(input("Masukan Nominal Yang Ingin Ditarik : ")));
+    }
+    public static boolean viewRepeatOption(){
+        String repeat = input("Apakah Anda ingin mengulang transaksi[y/N]?");
+        return handleOption(repeat);
+    }
+}
